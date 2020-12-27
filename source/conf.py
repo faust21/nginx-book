568b567713acf87fd5fafc5863df8b9240204bd7
Yohanan <gx24601@outlook.com>
2020-12-27 15:02:21 +0800

    pngmath->imgpath

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ TITLE = u"Nginx开发从入门到精通"
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 sys.path.append(os.path.abspath(_exts))
-extensions = ['sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'number_ref',
+extensions = ['sphinx.ext.todo', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig', 'number_ref',
               'number_label', 'literal_include', 'block', 'image', 'basic', "latex_fix"]
 
 #extensions.append('nohighlight')
